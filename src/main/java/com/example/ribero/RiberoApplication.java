6a623c9cfa2dcc6dc9e5d1f779d00c3a1e905e44
team-libero <ma-kobayashi@ac-tec.co.jp>
2023-02-15 17:41:10 +0900

    Second commit.

---
+++ src/main/java/com/example/ribero/RiberoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ribero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiberoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiberoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ribero/RiberoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ribero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiberoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
